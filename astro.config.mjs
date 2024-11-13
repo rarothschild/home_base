6be27e2b03c49f261c8b7fa050cae0734000e717
RobRothschild <rrothschild@helio.space>
2024-11-13 14:41:10 -0800

    Updating Portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://robrothschild.com',
   integrations: [tailwind()],
 });
