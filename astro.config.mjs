dd508a626b0b074e1b3128e3969784665fe6e7f4
RobRothschild <rrothschild@helio.space>
2024-11-13 15:06:17 -0800

    vercel configuating

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercelStatic from '@astrojs/vercel/static';
 
 export default defineConfig({
   site: 'https://robrothschild.com',
   integrations: [tailwind()],
+  // Vercel things
+  output: 'static',
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
